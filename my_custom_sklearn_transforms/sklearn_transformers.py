ff9dcb773d5692bada8e84bd33d319b3e22bd4d0
Igorxp5 <rogixp5@gmail.com>
2020-08-21 23:55:38 -0300

    ola

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -8,9 +8,29 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class SplitColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        for col in self.columns:
+            min_ = data[col].min()
+            max_ = data[col].max()
+            for i in range(min_, max_ + 1):
+                data['{}_{}'.format(i, col)] = (data[col] == i).apply(int)
+                data['{}_{}'.format(i, col)] = (data[col] == i).apply(int)
+                data['{}_{}'.format(i, col)] = (data[col] == i).apply(int)
+        return data.drop(columns=self.columns)
